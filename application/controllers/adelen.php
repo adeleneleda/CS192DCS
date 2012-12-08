3c43411401c609b3c38899590f2cb12e3e0a9ad0
Adelen Victoria Po Festin <festinadelen@gmail.com>
2012-12-08 17:55:20 +0800

    YAY

---
+++ application/controllers/adelen.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) die();
+class Adelen extends Main_Controller {
+   public function index()
+	{
+      $this->load->view('include/header');
+      $this->load->view('frontpage');
+      $this->load->view('include/footer');
+	}
+}
+
+/* End of file frontpage.php */
+/* Location: ./application/controllers/frontpage.php */
