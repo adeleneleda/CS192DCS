72f9e5f072bfc1446d1c092d35bb236470655eee
Matthew <matthewdeeco@gmail.com>
2013-03-20 20:01:10 +0800

    Fix last name parsing

--- application/models/fields/lastname.php
+++ application/models/fields/lastname.php
@@ -3,14 +3,14 @@ require_once 'field.php';
 
 class Lastname extends Field {
 	public function parse(&$lastname, $a = null, $b = null) {
-		$lastname = trim($lastname);
+		$lastname = strtoupper(trim($lastname));
 		if (empty($lastname))
 			throw new Exception("Last name is empty");
 		else if (preg_match('/[^\-a-zA-Z\. ]/u', $lastname))
 			throw new Exception("Last name contains non-alphabetic characters");
 		else if (strlen($lastname) > 45)
 			throw new Exception("Last name is greater than 45 characters");
-		$this->values['lastname'] = strtoupper($lastname);
+		$this->values['lastname'] = $lastname;
 	}
 }
 ?>
